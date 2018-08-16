e0d8b3757dd50746795977091f6bc7da1f079d95
dylan_DPC <dylan.dpc@gmail.com>
2018-08-17 02:02:54 +0530

    add skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
-#![feature(custom_attribute)]
-
 extern crate clap;
 extern crate clap_verbosity_flag;
 extern crate clap_log_flag;
 #[macro_use]
 extern crate structopt;
 
-use clap::{App, SubCommand};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
+
 struct Cli {
     #[structopt(flatten)]
     verbose: clap_verbosity_flag::Verbosity,
